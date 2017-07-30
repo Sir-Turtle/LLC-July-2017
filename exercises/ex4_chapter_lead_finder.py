9923c3477fb2b391d4a0618ef2eb7dd3ea1e528f
Sir-Turtle <tolgaakkaya@live.ca>
2017-07-30 10:29:54 -0400

    Exercise 4, read csv file and loop through dictionary for Ottawa LLC leads

--- exercises/ex4_chapter_lead_finder.py
+++ exercises/ex4_chapter_lead_finder.py
@@ -2,11 +2,12 @@
 import csv
 
 # Open the 'llc-chapters.csv' file
-
-# Convert it to a csv_data structure
-
-# Loop through each of the rows
-
-    # Compare the 'City' in the row with your city
-
-        # Print a thank you message to your chapter lead(s)
+with open('../exercises/llc-chapters.csv') as chapters_file:
+    # Convert it to a csv_data structure
+    chapters = csv.DictReader(chapters_file)
+    # Loop through each of the rows
+    for chapter in chapters:
+        # Compare the 'City' in the row with your city
+        if chapter['City'] == 'Ottawa':
+            # Print a thank you message to your chapter lead(s)
+            print('Thank you ' + chapter['Chapter Lead(s)'])
