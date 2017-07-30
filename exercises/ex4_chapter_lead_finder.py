167c1c084492c028f9f62c08953533372fc2933d
Sir-Turtle <tolgaakkaya@live.ca>
2017-07-29 23:57:58 -0400

    First commit, Ladies Learning Code Ottawa workshop material. Data Insights with Python for Beginners on July 29th

---
+++ exercises/ex4_chapter_lead_finder.py
@@ -0,0 +1,12 @@
+# Import the csv library
+import csv
+
+# Open the 'llc-chapters.csv' file
+
+# Convert it to a csv_data structure
+
+# Loop through each of the rows
+
+    # Compare the 'City' in the row with your city
+
+        # Print a thank you message to your chapter lead(s)
